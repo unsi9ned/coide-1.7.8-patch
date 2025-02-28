bcda536e0086d0876b3418307e4f0ff23c6ea9c7
Andrey Osipov <osandr@tut.by>
2025-02-28 10:26:15 +0300

    Optimized the flash programming driver. Faster memory programming speed.

--- flash/source/SAMD21G18/FlashAlgoFncs.c
+++ flash/source/SAMD21G18/FlashAlgoFncs.c
@@ -32,6 +32,9 @@ static const unsigned long pageSizes[] = { 8, 16, 32, 64, 128, 256, 512, 1024 };
 unsigned long base_adr;
 unsigned long pageSize;
 unsigned long pageNum;
+int flashErased = 0;
+
+extern struct FlashAlgorithm const FlashDriver;
 
 /**
  *******************************************************************************
@@ -53,176 +56,15 @@ int FlashInit(unsigned long baseAddr,
               unsigned long clk,
               unsigned long operateFuc)
 {
-#if 0
-	/* Set 1 Flash Wait State for 48MHz, cf tables 20.9 and 35.27 in SAMD21 Datasheet */
-	NVMCTRL->CTRLB.bit.RWS = NVMCTRL_CTRLB_RWS_HALF_Val;
-
-	/* Turn on the digital interface clock */
-	PM->APBAMASK.reg |= PM_APBAMASK_GCLK;
-
-	/* Software reset the module to ensure it is re-initialized correctly */
-	/* Note: Due to synchronization, there is a delay from writing CTRL.SWRST until the reset is complete.
-	 * CTRL.SWRST and STATUS.SYNCBUSY will both be cleared when the reset is complete, as described in chapter 13.8.1
-	 */
-	GCLK->CTRL.reg = GCLK_CTRL_SWRST ;
-
-	while ( (GCLK->CTRL.reg & GCLK_CTRL_SWRST) && (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) )
-	{
-		/* Wait for reset to complete */
-	}
-
-	/* ----------------------------------------------------------------------------------------------
-	 * 2) Put OSC8m/8 as source of Generic Clock Generator 1 and divide by 40 (25 kHz)
-	 */
-	GCLK->GENDIV.reg = GCLK_GENDIV_ID( 1 ) | // Generic Clock Generator 1
-					   GCLK_GENDIV_DIV(40);
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* Write Generic Clock Generator 1 configuration */
-	GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( 1 ) | // Generic Clock Generator 1
-						GCLK_GENCTRL_SRC_OSC8M | // Selected source is External 32KHz Oscillator
-						GCLK_GENCTRL_GENEN ;
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* ----------------------------------------------------------------------------------------------
-	 * 3) Put Generic Clock Generator 1 as source for Generic Clock Multiplexer 0 (DFLL48M reference)
-	 */
-	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID( 0 ) | // Generic Clock Multiplexer 0
-						GCLK_CLKCTRL_GEN_GCLK1 | // Generic Clock Generator 1 is source
-						GCLK_CLKCTRL_CLKEN ;
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-
-	/* ----------------------------------------------------------------------------------------------
-	 * 4) Enable DFLL48M clock
-	 */
-
-	/* DFLL Configuration in Closed Loop mode, cf product datasheet chapter 15.6.7.1 - Closed-Loop Operation */
-
-	/* Remove the OnDemand mode, Bug http://avr32.icgroup.norway.atmel.com/bugzilla/show_bug.cgi?id=9905 */
-	SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
-
-	while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-	{
-		/* Wait for synchronization */
-	}
-
-	SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP( 31 ) | // Coarse step is 31, half of the max value
-						   SYSCTRL_DFLLMUL_FSTEP( 511 ) | // Fine step is 511, half of the max value
-						   SYSCTRL_DFLLMUL_MUL( (VARIANT_MCK + VARIANT_MAINOSC/2) / VARIANT_MAINOSC ) ; // External 32KHz is the reference
-
-	while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* Write full configuration to DFLL control register */
-	SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_MODE | /* Enable the closed loop mode */
-						   SYSCTRL_DFLLCTRL_WAITLOCK |
-						   SYSCTRL_DFLLCTRL_QLDIS ; /* Disable Quick lock */
-
-	while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* Enable the DFLL */
-	SYSCTRL->DFLLCTRL.reg |= SYSCTRL_DFLLCTRL_ENABLE ;
-
-	while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLLCKC) == 0 ||
-		  (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLLCKF) == 0 )
-	{
-		/* Wait for locks flags */
-	}
-
-	while ( (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0 )
-	{
-		/* Wait for synchronization */
-	}
-
-
-	/* ----------------------------------------------------------------------------------------------
-	 * 5) Switch Generic Clock Generator 0 to DFLL48M. CPU will run at 48MHz.
-	 */
-	GCLK->GENDIV.reg = GCLK_GENDIV_ID( 0 ) ; // Generic Clock Generator 0
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* Write Generic Clock Generator 0 configuration */
-	GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( 0 ) | // Generic Clock Generator 0
-						GCLK_GENCTRL_SRC_DFLL48M | // Selected source is DFLL 48MHz
-						GCLK_GENCTRL_IDC | // Set 50/50 duty cycle
-						GCLK_GENCTRL_GENEN ;
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-	/* ----------------------------------------------------------------------------------------------
-	* 6) Modify PRESCaler value of OSC8M to have 8MHz
-	*/
 	SYSCTRL->OSC8M.bit.PRESC = SYSCTRL_OSC8M_PRESC_0_Val ;  //CMSIS 4.5 changed the prescaler defines
 	SYSCTRL->OSC8M.bit.ONDEMAND = 0 ;
 
-	/* ----------------------------------------------------------------------------------------------
-	* 7) Put OSC8M as source for Generic Clock Generator 3
-	*/
-	GCLK->GENDIV.reg = GCLK_GENDIV_ID( 3 ) ; // Generic Clock Generator 3
-
-	/* Write Generic Clock Generator 3 configuration */
-	GCLK->GENCTRL.reg = GCLK_GENCTRL_ID( 3 ) | // Generic Clock Generator 3
-						GCLK_GENCTRL_SRC_OSC8M | // Selected source is RC OSC 8MHz (already enabled at reset)
-						GCLK_GENCTRL_GENEN ;
-
-	while ( GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY )
-	{
-		/* Wait for synchronization */
-	}
-
-	/*
-	 * Now that all system clocks are configured, we can set CPU and APBx BUS clocks.
-	 * There values are normally the one present after Reset.
-	 */
-	PM->CPUSEL.reg  = PM_CPUSEL_CPUDIV_DIV1 ;
-	PM->APBASEL.reg = PM_APBASEL_APBADIV_DIV1_Val ;
-	PM->APBBSEL.reg = PM_APBBSEL_APBBDIV_DIV1_Val ;
-	PM->APBCSEL.reg = PM_APBCSEL_APBCDIV_DIV1_Val ;
-
-
-	/*
-	 * 9) Disable automatic NVM write operations
-	 */
-	NVMCTRL->CTRLB.bit.MANW = 1;	
-#else
-	/* ----------------------------------------------------------------------------------------------
-	* 6) Modify PRESCaler value of OSC8M to have 8MHz
-	*/
-	SYSCTRL->OSC8M.bit.PRESC = SYSCTRL_OSC8M_PRESC_0_Val ;  //CMSIS 4.5 changed the prescaler defines
-	SYSCTRL->OSC8M.bit.ONDEMAND = 0 ;
-
-	//9) Disable automatic NVM write operations
 	NVMCTRL->CTRLB.bit.MANW = 1;
-#endif
 	
 	base_adr = baseAddr;
 	pageSize = pageSizes[NVMCTRL->PARAM.bit.PSZ];
 	pageNum = NVMCTRL->PARAM.bit.NVMP;
+	flashErased = 0;
 
 	return (0);
 }
@@ -243,6 +85,7 @@ int FlashInit(unsigned long baseAddr,
  */
 int FlashUnInit(unsigned long operateFuc)
 {
+	flashErased = 0;
 	return (0);
 }
 
@@ -261,12 +104,27 @@ int FlashUnInit(unsigned long operateFuc)
  */
 int FlashEraseChip(void)
 {
-	unsigned long sectorsNum = pageNum / FLASH_ROW_SIZE;
-	unsigned long sectorSize = pageSize * FLASH_ROW_SIZE;
+#if 0
 	unsigned long i, addr;
+	unsigned long sectorSize = FlashDriver.deviceInfo.sectors[0].size;
+	unsigned long sectorsNum = pageNum * pageSize/ sectorSize;
 
 	for(i = 0, addr = 0; i < sectorsNum; i++, addr += sectorSize)
 		FlashEraseSector(addr);
+#else
+	unsigned long i, addr;
+	unsigned long sectorsNum = pageNum / FLASH_ROW_SIZE;
+	unsigned long sectorSize = pageSize * FLASH_ROW_SIZE;
+
+	for(i = 0, addr = 0; i < sectorsNum; i++, addr += sectorSize)
+	{
+		NVMCTRL->ADDR.reg = addr / 2;
+		NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
+		while (!NVMCTRL->INTFLAG.bit.READY)
+		{
+		}
+	}
+#endif
 
 	return (0);
 }
@@ -284,15 +142,35 @@ int FlashEraseChip(void)
  * @note 
  *******************************************************************************
  */
+#if 0
 int FlashEraseSector(unsigned long sectorAddr)
 {
-	NVMCTRL->ADDR.reg = sectorAddr / 2;
-	NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
-	while (!NVMCTRL->INTFLAG.bit.READY)
+	unsigned long i;
+	unsigned long realSectorSize = pageSize * FLASH_ROW_SIZE;
+	unsigned long realSectorsNum = FlashDriver.deviceInfo.sectors[0].size / realSectorSize;
+
+	for(i = 0; i < realSectorsNum; i++, sectorAddr += realSectorSize)
 	{
+		NVMCTRL->ADDR.reg = sectorAddr / 2;
+		NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
+		while (!NVMCTRL->INTFLAG.bit.READY)
+		{
+		}
 	}
 	return (0);
 }
+#else
+// Из-за некорректной работы программы coflash стираем всю память каждый раз
+int FlashEraseSector(unsigned long sectorAddr)
+{
+	if(!flashErased)
+	{
+		FlashEraseChip();
+		flashErased = 1;
+	}
+	return 0;
+}
+#endif
 
 /**
  *******************************************************************************
@@ -313,14 +191,11 @@ int FlashProgramPage(unsigned long pageAddr,
                      unsigned long size,
                      unsigned char *buf)
 {
+	unsigned long i;
 	unsigned long page;
 	unsigned long * Flash;
 
 	Flash = (unsigned long *) pageAddr;
-	page = (pageAddr - base_adr) / FLASH_PAGE_SIZE_BYTE;
-
-	// Disable automatic page write
-	NVMCTRL->CTRLB.bit.MANW = 1;
 
 	// Do writes in pages
 	while (size)
@@ -331,10 +206,17 @@ int FlashProgramPage(unsigned long pageAddr,
 		{
 		}
 
-		// Copy to the Write Buffer
-		for (size = (size + 3) & ~3; size; size -= 4, buf += 4)
+		// Fill page buffer
+		for (i = 0, size = (size + 3) & ~3;
+			 i < FLASH_PAGE_SIZE_BYTE && size;
+			 i += 4, buf += 4)
 		{
 			*Flash++ = *((unsigned long *)buf);
+
+			if(size >= 4)
+				size -= 4;
+			else
+				size = 0;
 		}
 
 		// Execute "WP" Write Page
@@ -367,7 +249,7 @@ int FlashVerify(unsigned long verifyAddr,
                 unsigned char *buf)
 {
 	// TODO: your code for the page verify
-	int i;
+	unsigned long i;
 	unsigned char* pageBuf = (unsigned char*)verifyAddr;
 
 	for(i = 0; i < size; i++)
--- flash/source/SAMD21G18/FlashAlgoInfo.c
+++ flash/source/SAMD21G18/FlashAlgoInfo.c
@@ -16,89 +16,6 @@
 
 #define FLASH_ALGORITHM_VERSION 0x0030
 
-/////////////////////////////////////////////////////////////////////////////
-
-#if defined ( SAM3U_B1_128 )
-#if defined ( __GNUC__   )
-__attribute__ ((section(".driver_info")))
-#endif
-struct FlashAlgorithm const FlashDriver = {
-	((FLASH_FRAMEWORK_VERSION << 16) | FLASH_ALGORITHM_VERSION),
-	"ATSAM3U Bank1 128KB Flash",
-	{
-		FLASH_TYPE_ONCHIP,
-		0x00100000,                 // Device Start Address
-		0x00020000,                 // Device Size in Bytes (256kB)
-		256,                        // Programming Page Size
-		0xFF,                       // Initial Content of Erased Memory
-		{
-			{ 0x0100, 0x00000000 },
-			{ FLASH_SECTOR_END   },
-		},
-	},
-	5000,												// Time Out of FlashInit/FlashUnInit/FlashVerify/FlashBlankCheck function (in msec)
-	5000,    											// Time Out of FlashProgramage  Function (in msec)
-	5000,  												// Time Out of FlashEraseSector Function (in msec)
-	5000, 												// Time Out of FlashEraseChip   Function (in msec)
-};
-#endif
-
-/////////////////////////////////////////////////////////////////////////////
-
-#if defined ( SAM3U_B0_128 )
-#if defined ( __GNUC__   )
-__attribute__ ((section(".driver_info")))
-#endif
-struct FlashAlgorithm const FlashDriver = {
-	((FLASH_FRAMEWORK_VERSION << 16) | FLASH_ALGORITHM_VERSION),
-	"ATSAM3U Bank0 128KB Flash",
-	{
-		FLASH_TYPE_ONCHIP,
-		0x00080000,                 // Device Start Address
-		0x00020000,                 // Device Size in Bytes (128kB)
-		256,                        // Programming Page Size
-		0xFF,                       // Initial Content of Erased Memory
-		{
-			{ 0x0100, 0x00000000 },
-			{ FLASH_SECTOR_END   },
-		},
-	},
-	5000,												// Time Out of FlashInit/FlashUnInit/FlashVerify/FlashBlankCheck function (in msec)
-	5000,    											// Time Out of FlashProgramage  Function (in msec)
-	5000,  												// Time Out of FlashEraseSector Function (in msec)
-	5000, 												// Time Out of FlashEraseChip   Function (in msec)
-};
-#endif
-
-/////////////////////////////////////////////////////////////////////////////
-
-#if defined ( SAM3U_B0_64 )
-#if defined ( __GNUC__   )
-__attribute__ ((section(".driver_info")))
-#endif
-struct FlashAlgorithm const FlashDriver = {
-	((FLASH_FRAMEWORK_VERSION << 16) | FLASH_ALGORITHM_VERSION),
-	"ATSAM3U Bank0 64KB Flash",
-	{
-		FLASH_TYPE_ONCHIP,
-		0x00080000,                 // Device Start Address
-		0x00010000,                 // Device Size in Bytes (64kB)
-		256,                        // Programming Page Size
-		0xFF,                       // Initial Content of Erased Memory
-		{
-			{ 0x0100, 0x00000000 },
-			{ FLASH_SECTOR_END   },
-		},
-	},
-	5000,												// Time Out of FlashInit/FlashUnInit/FlashVerify/FlashBlankCheck function (in msec)
-	5000,    											// Time Out of FlashProgramage  Function (in msec)
-	5000,  												// Time Out of FlashEraseSector Function (in msec)
-	5000, 												// Time Out of FlashEraseChip   Function (in msec)
-
-};
-#endif
-
-/////////////////////////////////////////////////////////////////////////////
 #if defined ( __GNUC__   )
 __attribute__ ((section(".driver_info")))
 #endif
@@ -109,10 +26,10 @@ struct FlashAlgorithm const FlashDriver = {
 		FLASH_TYPE_ONCHIP,
 		0x00000000,                        // Device Start Address
 		0x00040000,                        // Device Size in Bytes (256kB)
-		64,                                // Programming Page Size TODO уточнить
+		1024,                              // Programming Page Size (real size = 64)
 		0xFF,                              // Initial Content of Erased Memory
 		{
-			{ 0x0100, 0x00000000 },
+			{ 4096, 0x00000000 },
 			{ FLASH_SECTOR_END   },
 		},
 	},
